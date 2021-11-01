c5d8c1c6b99e15b6a821274cce170e55abb534fa
syedahamedgsm <92162215+syedahamedgsm@users.noreply.github.com>
2021-11-01 20:18:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function home() {
+  document.getElementsByClassName("nav-link")[0].setAttribute("class", "nav-link active me-3");
+  document.getElementsByClassName("nav-link")[2].setAttribute("class", "nav-link me-3");
+  document.getElementsByClassName("nav-link")[4].setAttribute("class", "nav-link me-3");
+  document.getElementsByClassName("nav-link")[6].setAttribute("class", "nav-link me-3");
+}
+function features() {
+  document.getElementsByClassName("nav-link")[0].setAttribute("class", "nav-link me-3");
+  document.getElementsByClassName("nav-link")[2].setAttribute("class", "nav-link active me-3");
+  document.getElementsByClassName("nav-link")[4].setAttribute("class", "nav-link me-3");
+  document.getElementsByClassName("nav-link")[6].setAttribute("class", "nav-link me-3");
+}
+function pricing() {
+  document.getElementsByClassName("nav-link")[0].setAttribute("class", "nav-link me-3");
+  document.getElementsByClassName("nav-link")[2].setAttribute("class", "nav-link me-3");
+  document.getElementsByClassName("nav-link")[4].setAttribute("class", "nav-link active me-3");
+  document.getElementsByClassName("nav-link")[6].setAttribute("class", "nav-link me-3");
+}
+function about() {
+  document.getElementsByClassName("nav-link")[0].setAttribute("class", "nav-link me-3");
+  document.getElementsByClassName("nav-link")[2].setAttribute("class", "nav-link me-3");
+  document.getElementsByClassName("nav-link")[4].setAttribute("class", "nav-link me-3");
+  document.getElementsByClassName("nav-link")[6].setAttribute("class", "nav-link active me-3");
+}
